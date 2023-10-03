5f8ee46e8e5385ecd6543253e81446e29ad95d07
NelHim <n.himbaza@alustudent.com>
2023-10-03 18:47:15 +0200

    Feat: Create navbar for VanLife

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='p-8'>
+        <h1>Hello, I'm learning React Router</h1>
+    </div>
+  )
+}
+
+export default Home
